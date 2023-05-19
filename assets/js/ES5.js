e99ca2cad5247fbe1d49707c725eb2a07b1e769a
JonathanCastr <98371104+JonathanCastr@users.noreply.github.com>
2023-05-18 22:29:29 -0400

    Ultimos cambios en Index y ES5.js solo restan acciones finales para el trabajo

--- assets/js/ES5.js
+++ assets/js/ES5.js
@@ -1,19 +1,17 @@
-const {userForm, workerNameInput, rutInput, jobInput} = window;
+const {registerWorker, nameworker, rut, corporateJob,formRegisterNewProject,nameProject,workers} = window;
 
 //const worker1=new Worker(workerNameInput.value, rutInput.value, jobInput.value);
 
-function Worker(workerName,rut,corporateJob){
-    this.workerName=workerName;
+function Worker(nameworker,rut,corporateJob){
+    this.nameworker=nameworker;
     this.rut=rut;
     this.corporateJob=corporateJob;
-
 }
 
-function Project(name_project, workers){
- this.name_project = name_project;
-//  this.workers = workers;
-        
-    }
+function Project(projectName, workers){
+    this.projectName = projectName;
+    this.workers = workers;
+    
     Object.defineProperties(this, 'workers',{
         get: function(){
             return this.workers;
@@ -22,6 +20,7 @@ function Project(name_project, workers){
             this.workers = workers;
         }
     });
+}    
 Project.prototype.obtenerTodosLosTrabajadores = function(){
     let lista_trabajadores = this.trabajadores;
     for (let i = 0; i < lista_trabajadores.length; i++) {
@@ -29,13 +28,15 @@ Project.prototype.obtenerTodosLosTrabajadores = function(){
     }
 }
 
-const trabajador1 = new Worker('juanito','1233','operador')
+const trabajador1 = new Worker(nameworker.value,rut.value,corporateJob.value)
 const trabajador2 = new Worker('Pedrito','1234','operador')
 const trabajador3 = new Worker('jorgito','1235','jefe')
+
 
 const project1 = new Project('impresion de poleras', [trabajador1,
  trabajador2, trabajador3])
 
 console.log('trabajador: ', project1.obtenerTodosLosTrabajadores())
+
 
 
