a6602746dbe99f8b9d2d1ddfabade280190a84ab
galiardi <66950896+galiardi@users.noreply.github.com>
2023-05-19 11:30:32 -0400

    Guarda proyecto en el localStorage

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// DOM elements
 const {
   registerWorker,
   nameworker,
@@ -6,29 +7,26 @@ const {
   formRegisterNewProject,
   nameProject,
   workers,
+  workersSelector,
 } = window;
+
+// data
 
 const workerList = localStorage.getItem('workerList')
   ? JSON.parse(localStorage.getItem('workerList'))
   : [];
 
-registerWorker.addEventListener('submit', (e) => {
-  e.preventDefault();
+const projectList = localStorage.getItem('projectList')
+  ? JSON.parse(localStorage.getItem('projectList'))
+  : [];
 
-  const worker = new Worker(nameworker.value, rut.value, corporatejob.value);
-  workerList.push(worker);
-  localStorage.setItem('workerList', JSON.stringify(workerList));
+// constructor functions
 
-  // limpia formulario
-  nameworker.value= '';
-  rut.value = '';
-  corporatejob.value = '';
-});
-
-function Worker(workerName, rut, corporatejob) {
-  this.workerName = workerName;
+function Worker(name, rut, job) {
+  this.name = name;
   this.rut = rut;
-  this.corporateJob = corporatejob;
+  this.job = job;
+  this.id = String(Math.floor(Math.random() * 100000000));
 }
 
 function Project(projectName, workers) {
@@ -49,7 +47,71 @@ Project.prototype.getAllWorkers = function () {
   return this.workers;
 };
 
+// functions
 
 function createWorkersSelectorOnDOM() {
-
+  let checkboxInputs = '';
+  workerList.forEach((worker) => {
+    const { id, name, job } = worker;
+    checkboxInputs += `
+    <div>
+      <input type="checkbox" id="${id}" name="${name}" class="checkboxInput">
+      <label for="${id}">${name} (${job})</label>
+    </div>
+    `;
+  });
+  if (checkboxInputs === '') {
+    workersSelector.innerHTML = '<p>No hay trabajajadores registrados</p>';
+  } else {
+    workersSelector.innerHTML = checkboxInputs;
+  }
 }
+
+// forms listeners
+
+registerWorker.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const worker = new Worker(nameworker.value, rut.value, corporatejob.value);
+  workerList.push(worker);
+  localStorage.setItem('workerList', JSON.stringify(workerList));
+  createWorkersSelectorOnDOM();
+
+  // limpia formulario
+  nameworker.value = '';
+  rut.value = '';
+  corporatejob.value = '';
+});
+
+formRegisterNewProject.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // arreglo con los ids de los trabajadores selecionados
+  let selectedWorkersIds = [];
+  const inputs = document.querySelectorAll('.checkboxInput');
+  inputs.forEach((input) => {
+    const { id, checked } = input;
+    if (checked) {
+      selectedWorkersIds.push(id);
+    }
+  });
+
+  // objeto con los trabajadores indexados por id
+  const workerListById = workerList.reduce(
+    (acc, worker) => ({ ...acc, [worker.id]: worker }),
+    {}
+  );
+
+  // arreglo con los trabajadores seleccionados
+  const selectedWorkers = selectedWorkersIds.map((id) => workerListById[id]);
+
+  const project = new Project(nameProject.value, selectedWorkers);
+
+  projectList.push(project);
+
+  localStorage.setItem('projectList', JSON.stringify(projectList));
+
+  nameProject.value = '';
+});
+
+// start
+createWorkersSelectorOnDOM();
